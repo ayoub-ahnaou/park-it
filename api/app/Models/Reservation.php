65c4b7a5c675f4ca6f83601446cb16f984cc36ef
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-10 10:30:40 +0000

    Add Reservation model with relationships and fillable attributes

---
+++ api/app/Models/Reservation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "start_date",
+        "end_date",
+        "user_id",
+        "parking_id",
+        "price_total"
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parking()
+    {
+        return $this->belongsTo(Parking::class);
+    }
+}
