29a1585193b8190e4a89de0b41ac20e1ad15b5a5
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-10 15:31:58 +0000

    Refactor API routes to group user, parking, and reservation routes under authentication middleware provided by sanctum

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,24 +13,27 @@ Route::get('/', function () {
 // auth routes
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 // user routes
 Route::get('/parkings', [ParkingController::class, 'index']);
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{user}/past-reservations', [UserController::class, 'pastReservations']);
-Route::get('/users/{user}/current-reservations', [UserController::class, 'currentReservations']);
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/users/{user}/past-reservations', [UserController::class, 'pastReservations']);
+    Route::get('/users/{user}/current-reservations', [UserController::class, 'currentReservations']);
 
-// parking routes
-Route::post('/parkings', [ParkingController::class, 'store']);
-Route::get('/parkings/{parking}', [ParkingController::class, 'show'])->middleware('auth:sanctum');
-Route::put('/parkings/{parking}', [ParkingController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/parkings/{parking}', [ParkingController::class, 'destroy'])->middleware('auth:sanctum');
+    Route::get('/logout', [AuthController::class, 'logout']);
 
-// reservation routes
-Route::get('/reservations', [ReservationController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/reservations/{parking}', [ReservationController::class, 'store'])->middleware('auth:sanctum');
-Route::get('/reservations/{reservation}', [ReservationController::class, 'show'])->middleware('auth:sanctum');
-Route::put('/reservations/{reservation}', [ReservationController::class, 'update'])->middleware('auth:sanctum');
-Route::put('/reservations/{reservation}/cancel', [ReservationController::class, 'cancel'])->middleware('auth:sanctum');
-Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy'])->middleware('auth:sanctum');
+    // parking routes
+    Route::post('/parkings', [ParkingController::class, 'store']);
+    Route::get('/parkings/{parking}', [ParkingController::class, 'show']);
+    Route::put('/parkings/{parking}', [ParkingController::class, 'update']);
+    Route::delete('/parkings/{parking}', [ParkingController::class, 'destroy']);
+    
+    // reservation routes
+    Route::get('/reservations', [ReservationController::class, 'index']);
+    Route::post('/reservations/{parking}', [ReservationController::class, 'store']);
+    Route::get('/reservations/{reservation}', [ReservationController::class, 'show']);
+    Route::put('/reservations/{reservation}', [ReservationController::class, 'update']);
+    Route::put('/reservations/{reservation}/cancel', [ReservationController::class, 'cancel']);
+    Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy']);
+});
