f7309b7ba019729af4602b728ae80d54307c01ec
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-10 10:18:29 +0000

    Add API routes for authentication, parking, and reservations

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,2 +1,34 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ParkingController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json(['project' => 'Park\'it']);
+});
+
+// auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+// user routes
+Route::get('/parkings', [ParkingController::class, 'index']);
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{user}/reservations', [UserController::class, 'reservations']);
+
+// parking routes
+Route::post('/parkings', [ParkingController::class, 'store']);
+Route::get('/parkings/{parking}', [ParkingController::class, 'show'])->middleware('auth:sanctum');
+Route::put('/parkings/{parking}', [ParkingController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/parkings/{parking}', [ParkingController::class, 'destroy'])->middleware('auth:sanctum');
+
+// reservation routes
+Route::get('/reservations', [ReservationController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/reservations/{parking}', [ReservationController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/reservations/{reservation}', [ReservationController::class, 'show'])->middleware('auth:sanctum');
+Route::put('/reservations/{reservation}', [ReservationController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy'])->middleware('auth:sanctum');
