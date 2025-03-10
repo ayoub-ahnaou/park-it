779fb9381fcb9e199c43eaaaf3a7b92741e92daa
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-10 10:23:30 +0000

    Add Parking model with fillable attributes for name, city, zone, places, and price

---
+++ api/app/Models/Parking.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Parking extends Model
+{
+    protected $fillable = [
+        "name",
+        "city",
+        "zone",
+        "places",
+        "price"
+    ];
+}
