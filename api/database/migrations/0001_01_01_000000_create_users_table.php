8cb784f04f01bff73a539e779d3cba58f95e4d22
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-11 12:20:58 +0000

    Add foreign key constraint for role_id in users table migration

--- api/database/migrations/0001_01_01_000000_create_users_table.php
+++ api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
+            $table->foreignId("role_id")->constrained("roles")->default(2)->onDelete("cascade");
             $table->timestamps();
         });
 
