26557f466bf3c341a9d5fe3a8796f5599b43e438
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-11 12:21:28 +0000

    Add AdminMiddleware to restrict access based on user role

---
+++ api/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::user()->role_id == 2) return response()->json(["message" => "unauthorized"], 403);
+        return $next($request);
+    }
+}
