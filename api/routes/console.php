e18227abd4b5904d85b18b266d4bafa2e0281d52
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-12 12:01:02 +0000

    Add scheduled task to update expired reservations every 30 minutes

--- api/routes/console.php
+++ api/routes/console.php
@@ -1,10 +1,20 @@
 <?php
 
-use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    /** @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::call(function () {
+    Log::info('Checking for expired reservations...');
+    $reservations = Reservation::where("end_date", "<", now())->where("status", "active")->with("parking")->get();
+
+    foreach ($reservations as $reservation) {
+        $reservation->update(["status" => "expired"]);
+        $reservation->parking->places_disponible++;
+        $reservation->parking->save();
+    }
+
+    Log::info('Updated reservations to expired.');
+})->everyThirtyMinutes();
+
+// https://grok.com/share/bGVnYWN5_bd3ea5e2-4d3c-4933-86eb-b60f7eae8d54
