27f70358ab51658c66e25ad8cb60816f0bd4dfbf
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-03-13 19:57:51 +0000

    Add OpenAPI attributes for parking API documentation

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -1,7 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
 
+#[
+    OA\Info(version: "1.0.0", description: "Parking api documentation", title: "ParkIt"),
+    OA\Server(url: "http://127.0.0.1:8000/api", description: "local server"),
+    OA\SecurityScheme(securityScheme: "bearerAuth", type: "http", name: "Authorization", in: "header", scheme: "bearer"),
+]
 abstract class Controller
 {
     //
